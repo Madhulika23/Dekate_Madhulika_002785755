4e0c21354f12edb998e9a52924b86e59f86b698a
Madhulika23 <dekate.ma@northeastern.edu>
2022-10-01 13:12:37 -0400

    Created Data page that would store employee data in table

---
+++ src/model/Data.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author madhulikadekate
+ */
+public class Data {
+ 
+    private ArrayList<Product> history;
+    public Data(){
+        this.history = new ArrayList<Product>();
+        
+    }
+
+    public ArrayList<Product> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Product> history) {
+        this.history = history;
+    }
+    
+    public Product addNewData(){
+            Product product = new Product();
+            history.add(product);
+            return product;
+            
+    }
+    
+    
+}
